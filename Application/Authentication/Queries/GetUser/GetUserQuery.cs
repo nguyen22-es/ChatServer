440bbe17e8fe20692dfd8590d7bbf44540472bee
nguyen22-es <112994817+nguyen22-es@users.noreply.github.com>
2024-04-29 17:12:14 +0700

    fix listuser

--- Application/Authentication/Queries/GetUser/GetUserQuery.cs
+++ Application/Authentication/Queries/GetUser/GetUserQuery.cs
@@ -1,27 +1,34 @@
 ﻿
 using Application.Common.Interfaces;
+using Application.Dot;
+using AutoMapper;
 using ChatSrever.Domain.Entities;
 
 namespace Chatserver.Application.Queries;
 
-public class GetAllUsersQuery : IRequest<IEnumerable<User>>
+public class GetAllUsersQuery : IRequest<IEnumerable<UserDot>>
 {
 
-    public class GetAllUsersQueryHandler : IRequestHandler<GetAllUsersQuery, IEnumerable<User>>
+    public class GetAllUsersQueryHandler : IRequestHandler<GetAllUsersQuery, IEnumerable<UserDot>>
     {
         private readonly IApplicationDbContext _context;
-        public GetAllUsersQueryHandler(IApplicationDbContext context)
+        private readonly IMapper _mapper;
+        public GetAllUsersQueryHandler(IApplicationDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
-        public async Task<IEnumerable<User>> Handle(GetAllUsersQuery query, CancellationToken cancellationToken)
+        public async Task<IEnumerable<UserDot>> Handle(GetAllUsersQuery query, CancellationToken cancellationToken)
         {
             var UserList = await _context.Users.ToListAsync();
             if (UserList == null)
             {
                 return null;
             }
-            return UserList.AsReadOnly();
+          var  User = _mapper.Map <List<UserDot>>(UserList);
+
+
+            return User.AsReadOnly();
         }
     }
 }
